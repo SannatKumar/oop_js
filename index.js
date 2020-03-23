e37a84b9e483d3a2470fc3f003a705d9a227cae3
Raj Kumar <interplaytheheads@gmail.com>
2020-03-24 00:58:44 +0200

    function and constructors

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 
-const circle = {
-    radius: 1,
-    location: {
-        x:1,
-        y:1
-    },
-    draw: function() {
-        console.log('draw');
-    }
-};
 
+
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function() {
+            console.log('Draw');
+        }
+    };
+}
+
+const circle = createCircle(1);
 circle.draw();
+
+
+//Consructor Function
+//Naming convention starts with capital letter inside constructor function 
+
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function(){
+        console.log('Draw from COnstructor');
+    }
+}
